350ef94f78f3479f2d06d043e78f42dab05a9b42
RuminaYuki <donminecraft36@gmail.com>
2026-03-27 23:38:51 +0700

    FixedLag

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,16 +5,23 @@ public class UIManager : MonoBehaviour
 {
     public void ReloadScene()
     {
+        ResetTime();
         Scene current = SceneManager.GetActiveScene();
         SceneManager.LoadScene(current.name);
     }
     public void LoadScene(string sceneName)
     {
+        ResetTime();
         SceneManager.LoadScene(sceneName);
     }
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("Quit Game");
+    }
+    private void ResetTime()
+    {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f;
     }
 }
